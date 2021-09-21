397a4ab43ccb73d5acf100ffdf74ec608b0f3a8c
Motyak <anfossotommy@hotmail.fr>
2021-09-21 22:30:58 +0200

    client works

--- tcpclient.c
+++ tcpclient.c
@@ -1,7 +1,8 @@
-#include <stdlib.h>
+// #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
-#include <sys/types.h>
+// #include <sys/types.h>
 #include <sys/socket.h>
 
 #include <netinet/in.h>
@@ -9,5 +10,35 @@
 // gcc tcpclient.c
 int main()
 {
+    /* create a socket */
+    // AF_INET      IPv4 Internet protocols
+    // SOCK_STREAM  Provides sequenced, reliable, two-way, connection-based byte streams
+    // The protocol specifies a particular protocol to be used with the socket
+    int s = socket(AF_INET, SOCK_STREAM, 0);
+
+    /* specify an address for the socket */
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1337);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    int status = connect(s, (struct sockaddr*)&addr, sizeof(addr));
+
+    if(status == -1)
+    {
+        perror("Erreur connexion");
+        return -1;
+    }
+
+    // receive data from the server
+    char response[256];
+    recv(s, &response, sizeof(response), 0);
+
+    // print out the response
+    printf("%s", response);
+
+    // and then close the socket
+    close(s);
+
     return 0;
 }
